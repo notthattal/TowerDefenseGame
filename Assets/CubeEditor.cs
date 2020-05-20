806ba62ae7b110270b37f5e07ab2e23feeb914cd
Tal Erez <>
2020-05-20 14:42:42 -0700

    set up grid system for cubes

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,17 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
 
     [SerializeField] float gridSize = 10f;
-    // Update is called once per frame
+
+    TextMesh textMesh;
+
+    void Start()
+    {
+
+    }
+
     void Update()
     {
         Vector3 snapPos;
+
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.y = 0f;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
 
         transform.position = snapPos;
     }
